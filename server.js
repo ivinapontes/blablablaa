09817c46a2a6de5a787c52451014cde7f41e1ca1
Glitch (hello-express) <none>
2019-06-04 19:53:15 +0000

    🐏👙 Checkpoint ./server.js:4409256/1549

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+let nextIndex = bookings.length
 
 app.get("/", function(request, response){
   response.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -15,7 +16,46 @@ app.get("/", function(request, response){
 
 
 // TODO add your routes and helper functions here
+//Create a new booking
+app.post("/new", function(request, response){
+  let booking = request.body;
+  booking.id = nextIndex++;
+    bookings.push(booking);
+    response.status(201).json(bookings); 
+})
 
+
+
+// {"id":1,"title":"Mr","firstName":"Jimi","surname":"Hendrix"
+//   ,"email":"jimi@example.com","roomId":2,"checkInDate":"2017-11-21","checkOutDate":"2017-11-23"}
+// app.post("/messages", function(request, response){
+//   let message = request.body;
+//   message.id = nextIndex++;
+//   if (!message.text || !message.from){
+//     response.status(400).send('missing text or name')
+//     } else {
+//     messages.push(message);
+//     response.status(201).json(messages); 
+//   }
+// });
+
+
+// Read all bookings
+app.get("/bookings", function(request, response){
+  response.json(bookings);
+});
+
+// Read one booking, specified by an ID
+app.get("/booking/:id?", function(request, response){
+  let id = parseInt(request.params.id);
+  let booking = bookings.filter(message => message.id == id);
+  response.json(booking);
+
+});
+
+
+
+// Delete a booking, specified by an ID
 
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
